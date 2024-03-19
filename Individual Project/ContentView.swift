68e43a56eb386baac4037d39176719196f0d3f36
Leon Chan <cwhs20122372@gmail.com>
2024-03-19 23:53:46 +0800

    Initial Commit

---
+++ Individual Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Individual Project
+//
+//  Created by chan yuk han on 19/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Individual Project/Individual_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Individual_ProjectApp.swift
+//  Individual Project
+//
+//  Created by chan yuk han on 19/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct Individual_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
